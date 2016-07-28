b44c63d773a6dbb7a79e6873512926d8fe96887c
Drethin <a.hong@live.ca>
2016-07-27 22:21:15 -0400

    intial variables required

--- src/OTInterface.java
+++ src/OTInterface.java
@@ -4,4 +4,15 @@
  * Interfaces with user, prints and gets text.
  */
 public class OTInterface {
+    private int money;
+    private int oxen;
+    private int food;
+    private int ammo;
+    private int clothing;
+    private int misc;
+    boolean injured;
+
+    int eventCounter;
+    int turnNum;
+    String date;
 }
