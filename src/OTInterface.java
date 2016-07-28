0624304408fa4eaeb12263af39002f1b44e898c5
Drethin <a.hong@live.ca>
2016-07-27 22:23:13 -0400

    oops

--- src/OTInterface.java
+++ src/OTInterface.java
@@ -4,15 +4,5 @@
  * Interfaces with user, prints and gets text.
  */
 public class OTInterface {
-    private int money;
-    private int oxen;
-    private int food;
-    private int ammo;
-    private int clothing;
-    private int misc;
-    boolean injured;
 
-    int eventCounter;
-    int turnNum;
-    String date;
 }
--- src/OTLogic.java
+++ src/OTLogic.java
@@ -2,4 +2,15 @@
  * Created by Alex on 2016-07-27.
  */
 public class OTLogic {
+    private int money;
+    private int oxen;
+    private int food;
+    private int ammo;
+    private int clothing;
+    private int misc;
+    boolean injured;
+
+    int eventCounter;
+    int turnNum;
+    String date;
 }
